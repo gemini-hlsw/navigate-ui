ce96d61f854202991b5353b3fc1a28fdc58eab14
Hugo van Rijswijk <git@hugovr.nl>
2025-02-17 13:39:24 +0100

    Remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import './styles/main.scss';
-import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 import { loadDevMessages, loadErrorMessages } from '@apollo/client/dev';
